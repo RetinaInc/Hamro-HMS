29eb390875f84fa7fe79c8b5add8315996568b1a
SanishMaharjan <sanishmaharjan@lftechnology.com>
2018-02-16 16:16:28 +0545

    Heroku server configuration

--- front-end/components/pages/patients/patientCheckInPage.js
+++ front-end/components/pages/patients/patientCheckInPage.js
@@ -3,7 +3,7 @@ import React from 'react';
 export default class PatientCheckInPage extends React.Component {
     style = {
         textarea: {
-            resize: false
+            resize: 'none'
         },
         startVisitDropDown: {
             width: 200,
